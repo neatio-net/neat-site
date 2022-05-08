61b1d92e3edccad032ca67b9e86c991f9ca8a6d5
GSilviu <99123684+neaTizu@users.noreply.github.com>
2022-05-08 08:59:03 +0300

    Upload

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+
+  $(function () {
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    // AOS ANIMATION
+    AOS.init({
+      disable: 'mobile',
+      duration: 800,
+      anchorPlacement: 'center-bottom'
+    });
+
+
+    // SMOOTHSCROLL NAVBAR
+    $(function() {
+      $('.navbar a, .hero-text a').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 44
+        }, 1000);
+        event.preventDefault();
+      });
+    });    
+  });
+
+  // ARROWS
+  setTimeout(function(){
+    document.getElementById('arrows').classList.remove('hide');
+   }, 3000);
+
+
+    
+
